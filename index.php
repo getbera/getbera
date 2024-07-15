bc102cdd8302ef1a6bfecca97aee2cffd81f51a1
getbera <150325894+getbera@users.noreply.github.com>
2024-07-15 14:12:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+<title>Plan and Report</title>
+<meta content="" name="description">
+<meta content="" name="keywords">
+
+<link href="image/logo.png" rel="icon">
+
+<!-- Google Fonts -->
+
+<link href="assetss/vendor/aos/aos.css" rel="stylesheet">
+<link href="assetss/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assetss/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assetss/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="assetss/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assetss/vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="assetss/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="assetss/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top " style="background-color: #146278;height: 54px;">
+<div class="container d-flex align-items-center">
+<h5 class="logo me-auto"><a href="index.php" style="font-size:18px;font-family: Abyssinica SIL;">PRMIS</a></h5>
+
+<nav id="navbar" class="navbar">
+<ul>
+<li><a class="nav-link scrollto active" href="#hero"> መነሻ </a></li>
+<li><a class="nav-link scrollto" href="#about"> ስለ ቢሮ </a></li>
+<li><a class="nav-link scrollto" href="#services"> ተግባርና ኃላፊነት </a></li>
+
+<li><a class="nav-link scrollto" href="#contact"> አድራሻ </a></li>
+<li><a class="getstarted scrollto" href="loginintofile.php"> ይግቡ</a></li>
+</ul>
+<i class="bi bi-list mobile-nav-toggle"></i>
+</nav><!-- .navbar -->
+
+</div>
+</header><!-- End Header -->
+<section id="hero" class="d-flex align-items-center" style="background-color: #fff9fe;height: 470px;">
+
+<div class="container">
+<div class="row" >
+<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="20">
+<div class="d-flex justify-content-center justify-content-lg-start"style="padding-left: 0%;">
+<a href="#about" class="btn-get-started scrollto" >
+<h4 style="font-family: Abyssinica SIL; text-align: center;" >
+በአዲስ አበባ ከተማ አስተዳደር ፕላንና ልማት ቢሮ የተቋማት ቁልፍ አፈጻጸም አመልካች እቅድ እና የሪፖርት አስተዳደር መረጃ ስርዓት <br/><br/>
+</h4>
+
+<h5 style="font-family: Times New Roman; text-align: center;font-style: italic;" >
+Organizations Key Performance Indicator Planning and Reporting Management Information System <br/>
+</h5>
+</a>
+</div>
+</div>
+
+<div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
+
+<img src="image/kpp.png" class="img-fluid animated" alt="" style="width: 570px;">
+</div>
+
+</div>
+</div>
+
+</section><!-- End Hero -->
+
+<main id="main">
+
+
+<!-- ======= About Us Section ======= -->
+<section id="about" class="about">
+<div class="container" data-aos="fade-up">
+
+<div class="section-title">
+<h3> ስለ ቢሮ </h3>
+</div>
+
+
+
+
+<div class="row content">
+<div class="col-lg-6 pt-4 pt-lg-0">
+
+  <ul>
+<li style="font-size:23px;">
+   ተልዕኮ /Mission:
+ </li>
+
+<li>
+<i class="ri-check-double-line"></i>  
+ህብረተሰቡንና ባለድርሻ አካላትን በማሳተፍ ጥናትና ምርምር በማከናወን፣ የልማት ፕላን በማዘጋጀትና አፈጻጸማቸውን በመከታተል፣ በመቆጣጠር፣ በመገምገምና መመዘን፣ የፕላን መረጃዎችን በዘመናዊ ቴክኖሎጂ በማስተዳደር ከተማዋን በፕላን በመምራት እና ልማቷን በማፋጠን ነዋሪዎቿን ተጠቃሚ ማድረግ ነው፡፡
+</li>
+
+
+  
+<li style="font-size:23px;">
+ ራዕይ/Vision
+   </li>
+
+
+<li>
+<i class="ri-check-double-line"></i> 
+“በ2022 ዓ.ም አዲስ አበባ በዘመናዊ ፕላን የምትመራ የለማችና የበለፀገች ከተማ ሆና ማየት!”
+   </li>
+</ul>
+</div>
+<div class="col-lg-6 pt-4 pt-lg-0">
+<p style="text-align: justify;">
+  <ul>
+       <l style="font-size:23px;">
+ እሴቶች/Values
+    </l>
+ <li>
+<i class="ri-check-double-line"></i>  
+ አገልጋይነት፡- ለፕላንና ልማት ቢሮ በአዋጅ ከተሰጠው ተግባርና ኃላፊነት አንጻር ተቋማትንና የከተማውን ነዋሪዎች በታማኝነትና በቅልጥፍና ማገልገል፤
+</li>
+
+
+ <li>
+ <i class="ri-check-double-line"></i>  
+የሕግ የበላይነት፡- በህግ የፀደቀውን ፕላን በህጋዊ መንገድ ብቻ እንዲፈጸም ማድረግ፤
+</li>
+
+ <li>
+ <i class="ri-check-double-line"></i>  
+ተጠያቂነት፡- የቢሮውን ሥራዎች ከተቀመጠው ህግና መመሪያ ውጪ መፈጸም ተጠያቂነትን የሚያስከትል መሆኑን ለማመላከት ነው፤
+</li>
+
+ <li>
+ <i class="ri-check-double-line"></i>  
+አሳታፊነት፡- በልማት ፕላን ዝግጅትና ትግበራ ላይ የሚመለከታቸውን አካላት በማሳተፍ የባለቤትነት ስሜት በመፍጠር መሥራት አስፈላጊ መሆኑን ማመን ነው፤
+</li>
+
+ <li>
+ <i class="ri-check-double-line"></i>  
+ባለሙያዊነት፡- የቢሮውን ተልዕኮ በዕውቀትና በክህሎት ለማከናወን አመራሩና ሠራተኛው ራሱን ለማሻሻል ሁል ጊዜ የሚተጋ ማለት ነው፤
+</li>
+
+ <li>
+ <i class="ri-check-double-line"></i>  
+ለጥራት መትጋት፡- በፕላንና ልማት ቢሮ የሚዘጋጁ የማህበራዊ ኢኮኖሚና የስፓሻል ፕላኖች እንዲሁም የአፈጻጸም ክትትል፣ ቁጥጥር፣ ግምገማ፣ ምዘና፣ የጥናትና ምርምር እና የስታቲስቲክስ ሥራዎች በተቀመጠላቸው የጥራት ደረጃ መፈጸም ማለት ነው፤
+</li>
+
+ <li>
+ <i class="ri-check-double-line"></i>  
+ፍትሀዊነት፡- በቢሮው የሚሰጡ አገልግሎቶች ከአድሎአዊነት የጸዳ እንዲሆን ማድረግ ማለት ነው፤                                        
+</li>
+
+ <li>
+ <i class="ri-check-double-line"></i>  
+ተቋማዊ አጋርነት፡- የፕላንና ልማት ጉዳይ የአንድ ተቋም ጉዳይ ብቻ ባለመሆኑ የፕላንና ልማት አመራርና ሰራተኛ ተልዕኮን ለማሳካት ከተለያዩ ባለድርሻ አካላት ጋር በቅንጅትና በትብብር የሚሰራ መሆኑን የሚያመላክት ነው፤
+</li>
+</ul>
+</p>
+
+</div>
+</div>
+
+
+
+
+</div>
+</section><!-- End About Us Section -->
+
+
+<section id="services" class="services section-bg">
+<div class="container" data-aos="fade-up">
+
+<div class="section-title">
+<h3> ተግባርና ኃላፊነት </h3>
+<p>
+</p>
+</div>
+
+
+<div class="row content">
+<div class="col-lg-6">
+<ol>
+<li>
+
+ ከተማውን የአጭር፣ የመካከለኛና የረጅም ጊዜ ዕቅድ ያዘጋጃል፤ እንዲዘጋጅ ያደርጋል፤ አፈፃፀሙን ይከታተላል፤ ግብረ መልስ ይሰጣል፤ ይገመግማል፤ ይመዝናል፣ ደረጃ ያወጣል፣ ሪፖርት ለሚመለከተው ያቀርባል፤
+ </li><br/>
+<li>
+ጸድቆ በስራ ላይ በሚውለው ስትራቴጂክ እቅድ መሰረት በማድረግ በአስፈጸሚ ተቋማት የሚከናወኑ ነባርና አዲስ የካፒታል ፕሮጀክቶችን አፈፃፀም ይከታተላል፣ ይገመግማል፣ በአፈጻጸም ሂደት የሚያጋጥሙ ችግሮች እንዲፈቱ የመፍትሄ ኃሳብ ያስቀምጣል፣ ለሚመለከተው አካል ሪፖርት ያቀርባል፤ 
+ </li><br/>
+ <li>
+የከተማውን መዋቅራዊ ፕላን ያዘጋጃል፤ ያሻሽላል፣ ሲፀድቅም ጉዳዩ በሚመለከታቸው የፕላን አስፈጻሚ ተቋማት ተግባራዊ እንዲሆን የፕላን አፈጻጸም ክትትል ፣ ቁጥጥርና ኦዲት ያደርጋል፤ 
+ </li><br/>
+ <li>
+የአካባቢ ልማት ፕላን እና ዝርዝር የከተማ ዲዛይን ያዘጋጃል፤ እንዲዘጋጅ ያደርጋል፤ያሻሽላል ሲፀድቅም ጉዳዩ በሚመለከታቸው የፕላን አስፈጻሚ ተቋማት ተግባራዊ እንዲሆን የፕላን አፈጻጸም ክትትል ፣ ቁጥጥርና ኦዲት ያደርጋል፤
+ </li><br/>
+ <li>
+ የተቀናጀ የመሰረተ ልማት ፕላን ያዘጋጃል፤ ያፀድቃል፤ ተግባራዊ እንዲሆን ያደርጋል፤
+ </li><br/>
+ <li>
+ከተማ አቀፍ የጥናትና ምርምር ሥራዎች ያከናውናል፣ የፖሊሲ ሀሳቦችን ያመነጫል፣
+ </li><br/>
+ <li>
+ የከተማ ፕላን እና ስታስቲካል መረጃዎችን ይሰበስባል፣ ያቀነባብራል፤ ሰነድ ያዘጋጃል፣ ያሰራጫል፣ ያስተዳድራል፤
+ </li><br/>
+ <li>
+ በከተማ ፕላን ዝግጅት ወቅት የሕዝብ ተግባቦትና ስርፀት ሥራዎችን ያከናውናል፤ ዘመናዊ ቴክኖሎጂ በመጠቀም የመረጃ ቋት በማደራጀት፣ መረጃን ተደራሽ በማድረግ ጥቅም ላይ ያውላል፤
+ </li><br/>
+ <li>
+ ለከተማው አስተዳደር ተቋማት እና ሌሎች የፕላን ተጠቃሚ አካላት የከተማውን የልማት እቅድ እና የስፓሻል ፕላን በተመለከተ የድጋፍና የምክር አገልግሎት ይሰጣል፤
+ </li><br/>
+ <li>
+በወጡ ፖሊሲዎች፣ ህጎች ፣ የከተማ ፕላኖች፣ የልማት ፕሮግራሞችና ፕሮጀክቶች የአካል ጉዳተኞች፣ የአረጋዊያን፣ የሴቶችን፣ የወጣቶች፣ ህጻናት፣ የስነ- ህዝብ እና የአካባቢ ጉዳዮች በእቅድ ተካተው መፈጸማቸውን ያረጋግጣል፤
+ </li>
+</ol>
+</div>
+<div class="col-lg-6 pt-4 pt-lg-0">
+
+<ol start="11">
+
+<li>
+
+ፀድቆ ተግባር ላይ የዋለውን መዋቅራዊ እና የአካባቢ ልማት ፕላኖች መጠበቅ፣ ማልማትና ማስተዳደር ሲገባቸው ሀላፊነታቸውን ባልተወጡ አስፈጻሚ ተቋማት ላይ የእርምት እርምጃ ይወስዳል ወይም ህጋዊ እርምጃ እንዲወሰድ ያደርጋል፣
+ </li><br/>
+<li>
+ከተማ ፕላን ለማዘጋጀት የሚያስችል መሠረታዊ የከተማ ካርታ እና የሳተላይት ምስሎችን ከሀገሪቱ ጂኦዴቲክ ካርታ ጋር በተገናዘበ መልኩ ያዘጋጃል፤ እንዲዘጋጅ ያደርጋል፤ ያደራጃል፤ ያሰራጫል፤
+ </li><br/>
+ <li>
+ የሽንሻኖ ፕላኖችን፣ የይዞታ ማረጋገጫ ካርታዎች እና የመሰረተ ልማት ዲዛይኖችን የግንባታ ፈቃድ ሰነዶችን አፈፃፀም በሚመለከት ወቅታዊ ሪፖርቶችን ፕላን ፈፃሚና አስፈፃሚ አካላት እንዲያቀርቡ በማድረግ፣ በመስክ ምልከታ በማረጋገጥ የክትትልና ቁጥጥር ግምገማ ሪፖርት በማዘጋጀት ለሚመለከተው አካል የውሳኔ ሀሳብ ያቀርባል፤
+ </li><br/>
+ <li>
+የከተማውን ዝርዝር የሥነ-ህዝብ ጥናትና ትንበያ ያካሄዳል፣ ውጤቱንም ለከተማው አስተዳደር አካላትና የሌሎች ባለድርሻ አካላት ያሰራጫል፤ የስነህዝብ ፖሊሲ በከተማው ውስጥ ይፈፅማል፤ ያስፈፅማል፤
+ </li><br/>
+ <li>
+የከተማውን ዓመታዊ አጠቃላይ የምርት ዕድገት ለማስገኘት የሚያስፈልገውን የካፒታልና የምርታማነት ዕድገት መነሻዎችን ይገምታል፤ ውጤቱንም ለሚመለከታቸው የከተማው አስተዳደር አካላትና ለባለድርሻ አካላት ያሰራጫል፤
+ </li><br/>
+ <li>
+የከተማውን የምርት ዕድገት ግቦች የፖሊሲና የህግ ሪፎርም እርምጃዎች ጋር የተጣጣሙ የቁጠባ፣ የኢንቨስትመንትና የወጪና የገቢ ንግድ ከተማ አቀፍ የማክሮ ኢኮኖሚ ግቦችን ያዘጋጃል፤ ሲፀድቅም ተግባራዊ መሆኑን ይከታተላል፤
+ </li><br/>
+ <li>
+ከአጠቃላይ ከተማ አቀፍ ምርት የኢንዱስትሪ፣ የአገልግሎት ዘርፎችና የከተማ ግብርና የተናጠል ድርሻን ይለያል፤ ውጤቱንም ለሚመለከታቸው የከተማው አስተዳደር አካላትና ለባለድርሻ አካላት ያሰራጫል፤
+ </li><br/>
+ <li>
+ከተማውን በማኑፋክቸሪንግ ኢንዱስትሪና በአገልግሎት ዘርፎች መካከል የሚኖረውን የግብዓት አቅርቦት ፍላጎት ይተነትናል፤ ውጤቱንም ለሚመለከታቸው የከተማው አስተዳደር አካላትና ለባለድርሻ አካላት ያሰራጫል፤
+ </li><br/>
+ <li>
+ ከሚመለከታቸው የከተማው አስተዳደር አካላት ጋር ተከታታይ ምክክር በማድረግ የሚተገበሩ የልማት ፕሮግራሞችንና ፕሮጀክቶችን ይወስናል እንዲሁም ሊወሰዱ የሚገባቸውን የፖሊሲና የህግ ሪፎርም እርምጃዎችን የሚዘረዝር የመካከለኛና የረጅም ጊዜ ማህበራዊና ምጣኔ ሀብታዊ የዘርፍ ፕላን ያዘጋጃል፤ ሲፀድቅም ተግባራዊ መሆኑን ይከታተላል፤
+ </li><br/>
+ <li>
+በወጡ ፖሊሲዎች፣ ህጎች ፣ የከተማ ፕላኖች፣ የልማት ፕሮግራሞችና ፕሮጀክቶች የአካል ጉዳተኞች፣ የአረጋዊያን፣ የሴቶችን፣ የወጣቶች፣ ህጻናት፣ የስነ- ህዝብ እና የአካባቢ ጉዳዮች በእቅድ ተካተው መፈጸማቸውን ያረጋግጣል፤
+ </li>
+</ol>
+
+
+
+</div>
+</div>
+
+</div>
+</section><!-- End Services Section -->
+
+
+<section id="contact" class="contact">
+<div class="container" data-aos="fade-up">
+
+<div class="section-title">
+<h2>አድራሻ</h2>
+<p>
+
+</p>
+</div>
+
+<div class="row">
+
+<div class="col-lg-5 d-flex align-items-stretch">
+<div class="info">
+<div class="address">
+<i class="bi bi-geo-alt"></i>
+<h4>Location:</h4>
+<p>5 ኪሎ </p>
+</div>
+
+<div class="email">
+<i class="bi bi-envelope"></i>
+<h4>Email:</h4>
+<p>aaps@info.com</p>
+</div>
+
+<div class="phone">
+<i class="bi bi-phone"></i>
+<h4>Call:</h4>
+<p>+25111654342</p>
+</div>
+
+
+</div>
+
+</div>
+
+<div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+<form action="forms/contact.php" method="post" role="form" class="php-email-form">
+<div class="row">
+<div class="form-group col-md-6">
+<label for="name">Your Name</label>
+<input type="text" name="name" class="form-control" id="name" required>
+</div>
+<div class="form-group col-md-6">
+<label for="name">Your Email</label>
+<input type="email" class="form-control" name="email" id="email" required>
+</div>
+</div>
+<div class="form-group">
+<label for="name">Subject</label>
+<input type="text" class="form-control" name="subject" id="subject" required>
+</div>
+<div class="form-group">
+<label for="name">Message</label>
+<textarea class="form-control" name="message" rows="10" required></textarea>
+</div>
+<div class="my-3">
+<div class="loading">Loading</div>
+<div class="error-message"></div>
+<div class="sent-message">Your message has been sent. Thank you!</div>
+</div>
+<div class="text-center"><button type="submit">Send Message</button></div>
+</form>
+</div>
+
+</div>
+
+</div>
+</section><!-- End Contact Section -->
+
+</main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+
+<div class="container footer-bottom clearfix">
+<div class="copyright">
+&copy; Copyright <strong><span>PRMIS</span></strong>. All Rights Reserved
+</div>
+<div class="credits">
+
+Designed by <a href="#">AAITDB</a>
+</div>
+</div>
+</footer><!-- End Footer -->
+
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="assetss/vendor/aos/aos.js"></script>
+<script src="assetss/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assetss/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assetss/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assetss/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assetss/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="assetss/vendor/php-email-form/validate.js"></script>
+<script src="assetss/js/main.js"></script>
+
+</body>
+
+</html>
